73cb79cad0dcfd21e40d1293e78752bfefed001b
= <=>
2019-12-30 19:27:03 +0800

    Add javascript concatenate

---
+++ concatenate.js
@@ -0,0 +1,6 @@
+ console.log(2 + '2'); // 22 + is arithmatic and concatination operator
+
+ console.log(2 - '2'); // - is only arithmetic operator
+
+ console.log(1 < 2 < 3); // true 1<2  =true and true<3 true is 1 and 1<3
+ console.log(1 < 2 > 3); // false 1<2  =true and true<3 true is 1 and 1<3
